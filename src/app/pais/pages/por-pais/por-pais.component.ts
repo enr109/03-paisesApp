98434a1b3c5cb781e2c5182634caf1cf20db7da9
Enrique FR <enriquefranciscoramirez95@gmail.com>
2021-04-02 10:57:56 -0600

    RestCountries

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -6,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
 
-  constructor() { }
+  termino: string = 'Hola Mundo';
+  hayError: boolean = false;
 
-  ngOnInit(): void {
+  constructor( private PaisService: PaisService) { }
+
+  buscar(){
+    this.hayError = false;
+    console.log(this.termino);
+    this.PaisService.buscarPais( this.termino)
+      .subscribe( (resp)=>{
+        console.log(resp);
+      }, (err) => {
+        this.hayError= true;
+      });
   }
+  
+
+  
 
 }
