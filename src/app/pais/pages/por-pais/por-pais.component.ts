97b50e5c96fa1648807dd20e129d240aed952573
Enrique FR <enriquefranciscoramirez95@gmail.com>
2021-04-03 10:03:29 -0600

    Componente Input y Tabla

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,8 +10,9 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
 
-  termino: string = 'Hola Mundo';
+  termino: string = '';
   hayError: boolean = false;
+  paises  : Country[] = [];
 
   constructor( private PaisService: PaisService) { }
 
@@ -20,8 +22,10 @@ export class PorPaisComponent {
     this.PaisService.buscarPais( this.termino)
       .subscribe( (resp)=>{
         console.log(resp);
+        this.paises=resp;
       }, (err) => {
         this.hayError= true;
+        this.paises = [];
       });
   }
   
