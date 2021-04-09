06646baff1068c41e64b974edbe64b384e2463af
Enrique FR <enriquefranciscoramirez95@gmail.com>
2021-04-09 12:57:36 -0500

    Fin de la aplicasion

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { Country } from '../../interfaces/pais.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li {
+      cursor: pointer;
+    }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -13,10 +18,14 @@ export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises  : Country[] = [];
+  paisesSugeridos : Country[] = [];
+
+  mostarSugerencias: boolean = false;
 
   constructor( private PaisService: PaisService) { }
 
   buscar( termino: string){
+    this.mostarSugerencias= false;
     this.hayError = false;
     this.termino = termino;
     //console.log(this.termino);
@@ -33,6 +42,19 @@ export class PorPaisComponent {
 
   sugerencias(termino: string ){
     this.hayError = false;
+    this.termino= termino;
+    this.mostarSugerencias= true;
+
+    this.PaisService.buscarPais(termino)
+      .subscribe(paises=> this.paisesSugeridos=paises.splice(0,5),
+      (err) => this.paisesSugeridos=[]);
+
+  }
+
+
+  buscarSugerido( termino: string){
+    this.buscar(termino);
+    
   }
 
 }
--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [ `
+   button {
+     margin-right: 5px;
+   }
+  `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent  {
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia','europe','oceania'];
+  regionActiva: string = '';
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[]=[];
 
-  ngOnInit(): void {
+
+  constructor( private PaisService: PaisService) { }
+
+  getClaseCSS( region: string): string {
+    return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary';
   }
+
+
+  activarRegion( region: string ){
+
+    if ( region === this.regionActiva ) {return;}
+    this.regionActiva = region;
+    this.paises=[];
+    this.PaisService.buscarRegion( region )
+    .subscribe( paises => this.paises = paises);
+  }
+
+
+  /*buscar( termino: string){
+    this.hayError = false;
+    this.termino= termino;
+
+    this.PaisService.buscarRegion( this.termino )
+    .subscribe( (resp)=>{
+      console.log(resp);
+      this.paises=resp;
+    }, (err) => {
+      this.hayError=true;
+      this.paises=[];
+    } )
+    
+  }*/
+
+
+  
+
+  
 
 }
